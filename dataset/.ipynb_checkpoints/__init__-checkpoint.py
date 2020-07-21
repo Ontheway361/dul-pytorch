72f161c77acc70f86cb3aa45bfd87d949bf25b2d
lujie <lujie@akulaku.com>
2020-07-21 14:04:33 +0000

    speedup the verification

--- dataset/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from .lfw    import LFW
-from .database import DataBase
-from .verify import VerifyFace
-
--- model/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-#-*- coding:utf-8 -*-
-from .faceloss import FaceLoss
-from .dul_resnet import dulres_zoo
-from .fc_layer import FullyConnectedLayer
